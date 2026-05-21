edebac1b3ec26730173b39749228317450a55d32
dorielezaj-creator <dorielezaj@gmail.com>
2026-05-21 11:13:43 +0200

    Update create-booking.js

--- api/create-booking.js
+++ api/create-booking.js
@@ -2,6 +2,9 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
   return res.status(200).json({ ok: true });
 }
+  if (req.method === 'HEAD') {
+  return res.status(200).end();
+}
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
